6beef3e1e67db2e9ecbca9abbbf72292bd03e4c1
rebecca <rebeccaesyria@gmail.com>
2018-02-11 02:25:48 -0500

    added wall collision to walk script

--- Assets/Scripts/Walking.cs
+++ Assets/Scripts/Walking.cs
@@ -6,33 +6,45 @@ public class Walking : MonoBehaviour {
 Rigidbody2D m_Rigidbody;
 SpriteRenderer m_SpriteRenderer;
 float m_Speed;
+bool canMove;
+bool canMoveLeft;
+bool canMoveRight;
     private bool initialize;
 	// Use this for initialization
 	void Start () {
 		m_Rigidbody = GetComponent<Rigidbody2D>();
 		m_SpriteRenderer = GetComponent<SpriteRenderer>();
 		m_Speed = 10.0f;
+    canMoveLeft = true;
+    canMoveRight = true;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		m_Speed = 20 * Input.GetAxis("Horizontal");
-		m_Rigidbody.velocity = transform.right * m_Speed;
-		if( m_Speed < 0){
+    if( m_Speed < 0 && canMoveLeft){
+      m_Rigidbody.velocity = transform.right * m_Speed;
 			m_SpriteRenderer.flipX = true;
-		}else if(m_Speed > 0){
+		}else if(m_Speed > 0 && canMoveRight){
+      m_Rigidbody.velocity = transform.right * m_Speed;
 			m_SpriteRenderer.flipX = false;
 		}
 	}
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.gameObject.name);
         if (collision.gameObject.CompareTag("Wall"))
         {
             Debug.Log("Hit Wall");
-            m_Rigidbody.velocity = Vector2.zero;
+            canMoveRight = false;
         }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision){
+      if(collision.gameObject.CompareTag("Wall")){
+        canMoveRight = true;
+      }
     }
 
 }
