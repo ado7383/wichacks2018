e2d4daccc579498de1787f38da66c7da298e22c0
Alyssa Oster <ado7383@rit.edu>
2018-02-11 06:47:42 -0500

    Application exits upon completion

--- Assets/Scripts/SleepingCat.cs
+++ Assets/Scripts/SleepingCat.cs
@@ -5,13 +5,43 @@ using UnityEngine;
 public class SleepingCat : MonoBehaviour {
 
     public int dayToAppear;
+    private bool gameEnd;
+    private int count;
+    private GameObject tracker;
+
     private void Start()
     {
-        GameObject tracker = GameObject.FindGameObjectWithTag("Track");
+        gameEnd = false;
+        count = 0;
+        tracker = GameObject.FindGameObjectWithTag("Track");
         if(tracker.GetComponent<Track>().day != dayToAppear || 
             (tracker.GetComponent<Track>().lastDayReady == false && tracker.GetComponent<Track>().day == 11 ))
         {
             this.gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(tracker.GetComponent<Track>().lastDayReady)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                GameObject fader = GameObject.Find("ExitFade");
+                fader.GetComponent<Animator>().enabled = true;
+                gameEnd = true;
+            }
+        }
+    }
+    private void Update()
+    {
+        if (gameEnd)
+        {
+            count++;
+            if (count > 500)
+            {
+                Debug.Log(count);
+                Application.Quit();
+            }
         }
     }
 }
--- Assets/Scripts/VisibleCat.cs
+++ Assets/Scripts/VisibleCat.cs
@@ -1,21 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VisibleCat : MonoBehaviour {
     public int[] daysToNotShow;
+    private GameObject tracker;
 	// Use this for initialization
 	void Start ()
     {
-        GameObject tracker = GameObject.FindGameObjectWithTag("Track");
+        tracker = GameObject.FindGameObjectWithTag("Track");
         foreach (int i in daysToNotShow)
         {
             if(tracker.GetComponent<Track>().day == i)
             {
-                this.gameObject.SetActive(false);
+                //this.gameObject.SetActive(false);
                 tracker.GetComponent<Track>().fed = true;
             }
         }
 	}
-	
+
+
 }
